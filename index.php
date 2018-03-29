a0d95ffc70525c769e486ceedff7fd8cd0e11bfa
Derrick Mungai <derrickmungai@gmail.com>
2018-03-29 20:20:58 +0300

    Lets structure this better

--- index.php
+++ index.php
@@ -10,10 +10,9 @@
 include 'driver.php';
 $driver = new Driver();
 $data = json_decode(file_get_contents('php://input'), true);
-
 $id= $data["message"]["chat"]["id"];
 $text=$data["message"]["text"];
-//print_r($data);
+
 //sample data for the bot to use
 // change to json data later
 $products=array('Order parts','Request Service');
@@ -21,7 +20,7 @@ $make= array("Volvo", "BMW", "Toyota","Nissan","Subaru","Jaguar","Mitsubishi");
 $model=array(array("volvo1","volvo2"), array("BMW1","BMW2"),array("toyota1","toyota2"),array());
 $firstMarkup=array('keyboard' => array(array($products[0]),array($products[1])));
 
-print_r($firstMarkup);
+//print_r($firstMarkup);
 $replyMarkup = array(
     'keyboard' => array(
         array($make[0]),
@@ -35,7 +34,7 @@ $replyMarkup = array(
 );
 //death by if statements
 if ($text=='Order parts'){
-    $driver->sendmessage($id, "You selected:'$text', What is your car make,model and year?");
+    $driver->sendmessage($id, "Great! Tell us about your car?");
     $driver->send_custom_keyboard($id, "Make", json_encode($replyMarkup));
     
 }
@@ -43,8 +42,6 @@ elseif (in_array($text,$make)){
     $driver->sendmessage($id,"model");
 
 }
-
-
 else {
     // initial point where the text is not recognised by the if statement.
     $driver->send_custom_keyboard($id, "Which product would you like to use?", json_encode($firstMarkup));
